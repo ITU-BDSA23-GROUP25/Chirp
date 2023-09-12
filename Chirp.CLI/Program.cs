373f04e7342c231f99cdd4c3dbaedf910b363acd
Adam <aaab@itu.dk>
2023-09-12 11:40:24 +0200

    refactoring csv file reader
    co-author: Atila - atia@itu.dk
    co-author: Karl - kagl@itu.dk
    co-author: Sebastian - segb@itu.dk
    co-author: Silas - siwo@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // used for time converter: https://www.educba.com/timestamp-to-date-c-sharp/
 // used for CSV formatting https://www.csharptutorial.net/csharp-file/csharp-read-csv-file/
+
+/*
+* CSV helper coded. co-authored-by co-author: Adam - aaab@itu.dk
+* co-author: Karl - kagl@itu.dk
+* co-author: Sebastian - segb@itu.dk
+* co-author: Silas - siwo@itu.dk 
+*/
+
 using System.Text.RegularExpressions;
 using CsvHelper;
 using System.Globalization;
-
 class Cheep
 {
     public string? Author { get; set; }
