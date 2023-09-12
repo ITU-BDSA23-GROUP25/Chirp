f11bddeac31d60f6e40b423833188daac7b3bd08
adga12 <60175609+adga12@users.noreply.github.com>
2023-09-12 11:27:38 +0200

    refactoring csv file reader
    co-author: Adam - aaab@itu.dk
    co-author: Karl - kagl@itu.dk
    co-author: Sebastian - segb@itu.dk
    co-author: Silas - siwo@itu.dk
    "

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,62 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // used for time converter: https://www.educba.com/timestamp-to-date-c-sharp/
-
-
 using System.Text.RegularExpressions;
 
-if (args[0].Equals("read"))
-    showData();
-else if (args[0].Equals("cheep"))
-    postCheep(args[1]);
-else
-    Console.WriteLine("n");
 
-
-void showData()
 {
-    string[] line;
-    string regex = """([a-zA-Z0-9_-]+),"([a-zA-Z0-9_, -:.]+)",([0-9]+)""";
-    try
     {
-        StreamReader sr = new StreamReader("chirp_cli_db.csv");
-        //line = sr.ReadLine().Split(",");
-        string next = sr.ReadLine();
-        next = sr.ReadLine();
 
 
-        while (next != null)
         {
-            Match m = Regex.Match(next, regex);
-            if (m.Success)
             {
-                Console.WriteLine(m.Groups[1].Value + " @ " + timeConverter(Double.Parse(m.Groups[3].Value)) + ": " + $"\"{m.Groups[2].Value}\"");
 
-            } else {
 
-                Console.WriteLine("fejl");
             }
-            next = sr.ReadLine();
-        }
 
     }
-    catch (Exception e)
     {
-        Console.WriteLine(e.Message);
     }
-}
 
-string timeConverter(double timeStamp)
-{
-    DateTime sd = new(1970, 1, 1, 2, 0, 0, 0);
-    sd = sd.AddSeconds(timeStamp);
-    string w = sd.ToString("MM/dd/yy HH:mm:ss");
-    return w;
-}
 
-void postCheep(string cheep)
-{
-    string name = Environment.UserName;
-    string time = Convert.ToString(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
-    string csv = string.Format("{0},{1},{2}\n", name, "\"" + cheep + "\"", time);
-    File.AppendAllText("chirp_cli_db.csv", csv);
-}
