1a6ed993f4ea93392c92bf59fd0de67574f447ab
Atila <atia@itu.dk>
2023-09-14 10:26:35 +0200

    co-authored-by co-author: Adam - aaab@itu.dk co-author: Karl - kagl@itu.dk * co-author: Sebastian - segb@itu.dk * co-author: Silas - siwo@itu.dk

--- SimpleDB/Class1.cs
+++ SimpleDB/Class1.cs
@@ -1,6 +1,53 @@
-﻿namespace SimpleDB;
+﻿using System.Globalization;
+using CsvHelper;
 
-public class Class1
+namespace SimpleDB;
+
+
+public class Class1 : IDatabaseRepository<Cheep>
 {
+    public IEnumerable<Cheep> Read(int? limit = null)
+    {
+        using var reader = new StreamReader("../SimpleDB/chirp_cli_db.csv");
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
+        // read CSV file
+        var records = csv.GetRecords<Cheep>();
+
+        
+        return records;
+    }
+
+    public void displayCheeps(IEnumerable<Cheep> records)
+    {
+        // output
+        foreach (var r in records)
+        {
+            Console.WriteLine($"{r.Author}" + " @ " + $"{timeConverter(Double.Parse(r.Timestamp!))}" + ": " + $"{r.Message}");
+        }
+    }
+
+     public void Store(Cheep record)
+    {
+        string name = getUsername();
+        string time = getUNIXTime();
+        string csv = string.Format("{0},{1},{2}\n", name, "\"" + record + "\"", time);
+        File.AppendAllText("../SimpleDB/chirp_cli_db.csv", csv);
+    }
+
+        static string timeConverter(double timeStamp)
+    {
+        DateTime sd = new(1970, 1, 1, 2, 0, 0, 0);
+        sd = sd.AddSeconds(timeStamp);
+        string w = sd.ToString("MM/dd/yy HH:mm:ss");
+        return w;
+    }
+
+    static string getUNIXTime(){
+        return Convert.ToString(DateTimeOffset.UtcNow.ToUnixTimeSeconds());;
+    }
+
+    static string getUsername(){
+        return Environment.UserName;
+    }
 }
