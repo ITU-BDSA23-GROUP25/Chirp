5fa4f97899b3bffcc16e14238bdb17998b9d187e
Sebastian Blok <segb@itu.dk>
2023-12-05 12:01:47 +0100

    fixes Primary key problem

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -2,10 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Repository;
 
-public class Author : IdentityUser<string> //added <string>
+public class Author : IdentityUser
 {
-    public Guid Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
-    public List<Cheep> Cheeps { get; set; } = new();
+    public List<Cheep> Cheeps { get; set; } = new();  
 }
