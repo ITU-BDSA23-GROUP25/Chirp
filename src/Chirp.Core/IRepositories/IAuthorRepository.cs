7c3f2799bbaad102f1d3cd2565c6b2001133ea73
KarlFraITU <118450649+KarlFraITU@users.noreply.github.com>
2023-12-20 21:49:20 +0100

    have made comments for the chirp.core folder

--- src/Chirp.Core/IRepositories/IAuthorRepository.cs
+++ src/Chirp.Core/IRepositories/IAuthorRepository.cs
@@ -1,12 +1,34 @@
 namespace Core;
+
+/// <summary>
+/// This is the interface for the AuthorRepository, where the methodsignitures are displayed
+/// </summary>
 public interface IAuthorRepository
 {
     // Get
-    public Task<AuthorDTO> GetAuthorByName(string author_name);
+
+    /// <summary>
+    /// This method returns the AuthorDTO when given the authors name (string)
+    /// </summary>
+    /// <param name="name">The authors name in form of a string</param>
+    /// <returns></returns>
+    public Task<AuthorDTO> GetAuthorByName(string name);
 
     // post
+
+    /// <summary>
+    /// This method creates an author in the AspNetUsers table
+    /// only containing its name
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
     public Task CreateAuthor(string name);
 
+    /// <summary>
+    /// This method removes the author from the AspNetUsers table
+    /// </summary>
+    /// <param name="authorDTO"></param>
+    /// <returns></returns>
     public Task RemoveAuthor(AuthorDTO authorDTO);
 
 }
