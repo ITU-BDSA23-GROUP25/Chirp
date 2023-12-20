09f5065fb58e27981d9ac1fc8863d95322331635
KarlFraITU <118450649+KarlFraITU@users.noreply.github.com>
2023-12-20 22:50:19 +0100

    commented Entities

--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -2,7 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Repository;
 
-
+/// <summary>
+/// An Author inherrits from identityUser, which is an AspNetCore class
+/// used for authentication.
+/// An Author is the user and the owner of their cheeps
+/// </summary>
 public class Author : IdentityUser
 {
     public required string Name { get; set; }
--- src/Chirp.Infrastructure/Entities/Reaction.cs
+++ src/Chirp.Infrastructure/Entities/Reaction.cs
@@ -1,8 +1,11 @@
 namespace Repository;
 
+/// <summary>
+/// The Reaction entity is used for users to be able to react with a reactionType 
+/// to a given Cheep 
+/// </summary>
 public class Reaction
 {
-
     public required Guid CheepId { get; set; }
     public required string AuthorName { get; set; }
     public ReactionType ReactionType { get; set; }
