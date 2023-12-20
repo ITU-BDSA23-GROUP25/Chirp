bb488985be2e54cb4d555801d1b3c292c35038c9
Sebastian Blok <segb@itu.dk>
2023-12-21 00:55:26 +0100

    Adds test for AuthorRepository

--- test/Chirp.IntegrationTest/AuthorRepositoryTest.cs
+++ test/Chirp.IntegrationTest/AuthorRepositoryTest.cs
@@ -1,41 +1,53 @@
 namespace AuthorRepositoryTest;
 
+/// <summary>
+/// Integration testsuit for authorRepository
+/// </summary>
+
 public class AuthorRepositoryTest
 {
 
     private readonly IAuthorRepository _authorRepository;
-
-    private readonly ICheepRepository _cheepRepository;
     private readonly DatabaseContext _context;
 
     public AuthorRepositoryTest()
-    {
+    {   
+        // Set up an in-memory SQLite database for testing
         var connection = new SqliteConnection("DataSource=:memory:");
         connection.Open();
 
+        // Create a new DatabaseContext with the in-memory database and apply migrations
         var options = new DbContextOptionsBuilder<DatabaseContext>()
             .UseSqlite(connection)
             .Options;
 
+        
         _context = new DatabaseContext(options);
         _context.Database.Migrate();
         _context.InitializeDB();
 
+        // Initialize repositories with the in-memory database context
         _authorRepository = new AuthorRepository(_context);
-        _cheepRepository = new CheepRepository(_context);
     }
+
+    /// <summary>
+    /// Tests the creation and deletion of an author.
+    /// </summary>
 
     [Fact]
     public async void CreateAndDeleteAuthor()
     {
+        // Act: Create an author using the AuthorRepository
         var name = "Sebb";
-
+        
         var author = _authorRepository.CreateAuthor(name);
 
         var author_database = await _context.Authors.Where(c => c.Name == name).FirstOrDefaultAsync();
 
+        // Assert: Check if the created author matches the expected name
         Assert.Equal(author_database.Name, name);
 
+        // Act: Retrieve an AuthorDTO to remove the author
         var author_to_remove = await _context.Authors
             .Where(c => c.Name == name)
             .Select(a =>
@@ -46,6 +58,8 @@ public class AuthorRepositoryTest
 
         await _authorRepository.RemoveAuthor(author_to_remove);
 
+
+        // Assert: Check if the author is null, indicating successful removal
         author_database = await _context.Authors.Where(c => c.Name == name).FirstOrDefaultAsync();
 
         Assert.Null(author_database);
